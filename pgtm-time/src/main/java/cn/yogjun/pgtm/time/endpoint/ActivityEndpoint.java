d24d72b621c1a4286405af311e126659523ccf00
yogjun <280536928@qq.com>
2023-09-28 17:14:24 +0800

    add activity po

---
+++ pgtm-time/src/main/java/cn/yogjun/pgtm/time/endpoint/ActivityEndpoint.java
@@ -0,0 +1,10 @@
+package cn.yogjun.pgtm.time.endpoint;
+
+/**
+ * {@link ActivityEndpoint}
+ *
+ * @author <a href="mailto:280536928@qq.com">yogjun</a>
+ * @version ${project.version} - 2023/9/28
+ */
+public class ActivityEndpoint {
+}
